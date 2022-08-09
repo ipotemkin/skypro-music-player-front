2c95916a62a43251397eba4f9e282f9a83a499a1
Igor Potemkin <ipotemkin@rambler.ru>
2022-08-09 12:50:32 +0600

    feature: add Player

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,13 @@
-// import React from 'react';
-// import logo from './logo.svg';
-// import { Counter } from './features/counter/Counter';
-import { Routes } from './routes';
+// import { Routes } from './routes';
 import './App.css';
-import { Filter } from './features/Filter/Filter';
-import { FilterButton } from './features/FilterButton/FilterButton';
-import { Search } from './features/Search/Search';
-// import { LoginForm } from './features/LoginForm/LoginForm';
-// import { SideMenu } from './features/SideMenu/SideMenu';
-import { TrackType } from './features/Track/Track';
-// import { TrackList } from './features/TrackList/TrackList';
-import { Tracks } from './pages/Tracks/Tracks';
-// import { Tracks } from './pages/Tracks/Tracks';
-
-const sampleTrack: TrackType = {
-  title: 'Guilt',
-  author: 'Nero',
-  album: 'Welcome Reality',
-  time: '4:45'
-}
-
-const sampleTrack2: TrackType = {
-  title: 'Guilt2',
-  author: 'Nero2',
-  album: 'Welcome Reality2',
-  time: '4:45'
-}
-
-const sampleTrack3: TrackType = {
-  title: 'Guilt3',
-  author: 'Nero3',
-  album: 'Welcome Reality3',
-  time: '3:00'
-}
-
-
-const sampleTrackList: TrackType[] = [
-  sampleTrack,
-  sampleTrack2,
-  sampleTrack3
-]
+import { Player } from './features/Player/Player';
 
 function App() {
   return (
     <div className="App">
 
-      <Routes />
+      {/* <Routes /> */}
+      <Player />
 
       {/* <header className="App-header">
       </header> */}
---
+++ src/features/Player/Player.classname.ts
@@ -0,0 +1,5 @@
+import block from 'bem-cn-lite';
+
+export const cnPlayer = block('player');
+export const cnBar = block('bar');
+export const cnTrackPlay = block('track-play');
