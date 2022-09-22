c3c6e6f5d06a5828f667dc9489838071cfb1d410
Igor Potemkin <ipotemkin@rambler.ru>
2022-09-22 21:59:16 +0300

    refactor: remove showSidebar

--- src/features/Tracks/Tracks.tsx
+++ src/features/Tracks/Tracks.tsx
@@ -16,13 +16,13 @@ import './Tracks.css'
 type TracksProps = {
   title: string
   showFilter?: boolean
-  showSidebar?: boolean  // I left this variable till the final project submit
+  // showSidebar?: boolean  // I left this variable till the final project submit
   hook: Function  // set a hook to show playlists or collections
   collectionId?: number
 }
 
 export const Tracks: FC<TracksProps> = ({
-  title, showFilter = false, showSidebar = false, hook, collectionId
+  title, showFilter = false, hook, collectionId
 }) => {
   const [ searchString, setSearchString ] = useState('')
   const { isLoading, isError, data } = collectionId ? hook(searchString, collectionId) : hook(searchString)  
